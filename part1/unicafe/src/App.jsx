ebcaa79227e0a1eec6b8e6a64ae16a5c0c4e9864
Vili Sinervä <vili.m.sinerva@gmail.com>
2025-11-25 18:21:40 +0200

    Exercise 1.8 submission

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,21 +5,10 @@ const Title = ({text}) => <h1>{text}</h1>
 const FeedbackButton = ({value, setValue, text}) =>
   <button onClick={() => setValue(value + 1)}>{text}</button>
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
+const Statistics = ({good, neutral, bad}) => {
   const total = good + neutral + bad
-
   return (
-    <div>
-      <Title text="give feedback"/>
-      <FeedbackButton value={good} setValue={setGood} text="good" />
-      <FeedbackButton value={neutral} setValue={setNeutral} text="neutral" />
-      <FeedbackButton value={bad} setValue={setBad} text="bad" />
-
+    <>
       <Title text="statistics"/>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
@@ -27,6 +16,24 @@ const App = () => {
       <p>all {total}</p>
       <p>average {(good - bad) / total}</p>
       <p>positive {100 * good/ total} %</p>
+    </>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+  return (
+    <div>
+      <Title text="give feedback"/>
+      <FeedbackButton value={good} setValue={setGood} text="good" />
+      <FeedbackButton value={neutral} setValue={setNeutral} text="neutral" />
+      <FeedbackButton value={bad} setValue={setBad} text="bad" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
