35a98373cd12d4da2accf5c43cae255405f15fd6
Vili Sinervä <vili.m.sinerva@gmail.com>
2025-11-25 18:19:16 +0200

    Exercise 1.7 submission

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,13 +5,13 @@ const Title = ({text}) => <h1>{text}</h1>
 const FeedbackButton = ({value, setValue, text}) =>
   <button onClick={() => setValue(value + 1)}>{text}</button>
 
-const Feedback = ({value, text}) => <p>{text} {value}</p>
-
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const total = good + neutral + bad
 
   return (
     <div>
@@ -21,9 +21,12 @@ const App = () => {
       <FeedbackButton value={bad} setValue={setBad} text="bad" />
 
       <Title text="statistics"/>
-      <Feedback value={good} text="good"/>
-      <Feedback value={neutral} text="neutral"/>
-      <Feedback value={bad} text="bad"/>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {(good - bad) / total}</p>
+      <p>positive {100 * good/ total} %</p>
     </div>
   )
 }
