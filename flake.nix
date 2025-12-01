5fb2305fe725399890016acedf51da8d7490efdf
Vili Sinervä <vili@sinerva.eu>
2025-12-01 16:17:52 +0200

    Exercise 3.12 submission

--- flake.nix
+++ flake.nix
@@ -5,25 +5,24 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-      br-bg = pkgs.writeScriptBin "br-bg" "${pkgs.bruno}/bin/bruno 1>/dev/null 2>&1 &";
-      ff-bg = pkgs.writeScriptBin "ff-bg" "${pkgs.firefox-devedition}/bin/firefox-devedition 1>/dev/null 2>&1 &";
+      bg = cmd: "${pkgs.coreutils-full}/bin/nohup ${cmd} 1>/dev/null 2>&1 &";
+      bruno-bg = pkgs.writeScriptBin "br" (bg "${pkgs.bruno}/bin/bruno");
+      firefox-bg = pkgs.writeScriptBin "ff" (bg "${pkgs.firefox-devedition}/bin/firefox-devedition");
     in
     {
       # Utilized by `nix develop`
       devShell.${system} = pkgs.mkShell {
         buildInputs =
           (with pkgs; [
-            bruno
-            firefox-devedition
             nodejs_22
             openssl
             cacert
           ])
           ++ [
-            br-bg
-            ff-bg
+            bruno-bg
+            firefox-bg
           ];
-        shellHook = ''zsh'';
+        shellHook = "exec zsh";
       };
     };
 }
