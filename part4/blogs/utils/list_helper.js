d64fe47c931225e197d6a83ff1dfb85c2d8014f3
Vili Sinervä <vili@sinerva.eu>
2025-12-03 14:47:55 +0200

    Exercise 4.6 submission

--- part4/blogs/utils/list_helper.js
+++ part4/blogs/utils/list_helper.js
@@ -6,11 +6,33 @@ const totalLikes = (blogs) => blogs.reduce((sum, val) => sum+val.likes, 0)
 
 const favouriteBlog = (blogs) => {
 	const favourite = blogs.reduce((current, next) => current.likes >= next.likes ? current : next, {})
-	return (favourite._id ? favourite : undefined)
+	return favourite._id ? favourite : undefined
+}
+
+const mostBlogs = (blogs) => {
+	const blogCounts = blogs.reduce((values, blog) => {
+		const author = blog.author
+		let newValues = {...values}
+
+		if (!newValues[author])
+			newValues[author] = 0
+
+		newValues[author] += 1
+		return newValues
+	}, {})
+	const authorWithMostBlogs = Object.entries(blogCounts).reduce((current, next) => {
+		const nextObj = {
+			author: next[0],
+			blogs: next[1]
+		}
+		return current.blogs >= nextObj.blogs ? current : nextObj
+	}, {})
+	return authorWithMostBlogs.author ? authorWithMostBlogs : undefined
 }
 
 module.exports = {
 	dummy,
 	totalLikes,
-	favouriteBlog
+	favouriteBlog,
+	mostBlogs
 }
