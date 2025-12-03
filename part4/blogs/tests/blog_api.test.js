cf35b57dc0b16f0adf0a094301489a52904fb03a
Vili Sinervä <vili@sinerva.eu>
2025-12-03 16:11:31 +0200

    Exercise 4.9 submission

--- part4/blogs/tests/blog_api.test.js
+++ part4/blogs/tests/blog_api.test.js
@@ -22,6 +22,17 @@ test('all blogs are returned as json', async () => {
 	assert.strictEqual(response.body.length, helper.initialBlogs.length)
 })
 
+test('blog unique key is "id" ', async () => {
+	const response = await api
+		.get('/api/blogs')
+		.expect(200)
+		.expect('Content-Type', /application\/json/)
+
+	const blogProperties = Object.keys(response.body[0])
+	assert(blogProperties.includes('id'))
+	assert(!blogProperties.includes('_id'))
+})
+
 after(async () => {
 	await mongoose.connection.close()
 })
