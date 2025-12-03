0365a4e25de37d3ab5edf655a43de05b7fa64242
Vili Sinervä <vili@sinerva.eu>
2025-12-03 14:24:52 +0200

    Exercise 4.5 submission

--- part4/blogs/utils/list_helper.js
+++ part4/blogs/utils/list_helper.js
@@ -4,7 +4,13 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => blogs.reduce((sum, val) => sum+val.likes, 0)
 
+const favouriteBlog = (blogs) => {
+	const favourite = blogs.reduce((current, next) => current.likes >= next.likes ? current : next, {})
+	return (favourite._id ? favourite : undefined)
+}
+
 module.exports = {
 	dummy,
-	totalLikes
+	totalLikes,
+	favouriteBlog
 }
