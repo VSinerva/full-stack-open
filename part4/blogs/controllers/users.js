e7f436fe675fe4788fc5514ff72be77bcf2c456e
Vili Sinervä <vili@sinerva.eu>
2025-12-05 12:08:41 +0200

    Exercise 4.17 submission

--- part4/blogs/controllers/users.js
+++ part4/blogs/controllers/users.js
@@ -3,7 +3,7 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User.find({}).populate('blogs', { title: 1, author: 1, url: 1 })
   response.json(users)
 })
 
@@ -19,7 +19,8 @@ usersRouter.post('/', async (request, response) => {
   const user = new User({
     username,
     passwordHash,
-    name
+    name,
+    blogs: [],
   })
 
   const savedUser = await user.save()
